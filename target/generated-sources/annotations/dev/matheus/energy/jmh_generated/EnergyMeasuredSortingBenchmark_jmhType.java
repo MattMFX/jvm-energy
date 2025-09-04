f6dd89c15e11c6ec1e40a15def0e9af24240c2d6
Matheus <mfmachado0210@gmail.com>
2025-09-04 00:04:06 -0300

    feat: fixes

--- target/generated-sources/annotations/dev/matheus/energy/jmh_generated/EnergyMeasuredSortingBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package dev.matheus.energy.jmh_generated;
-public class EnergyMeasuredSortingBenchmark_jmhType extends EnergyMeasuredSortingBenchmark_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/dev/matheus/energy/jmh_generated/SortingBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package dev.matheus.energy.jmh_generated;
-public class SortingBenchmark_jmhType extends SortingBenchmark_jmhType_B3 {
-}
-
